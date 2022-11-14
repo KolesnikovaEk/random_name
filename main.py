2b6732b68c4c39e7228b8dd370c531e2266c48cf
kolesnikovaekaterina <Kolesnikova.Ek.21@yandex.ru>
2022-11-14 23:05:48 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import random
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from UI import Ui_Form
+from PyQt5.QtGui import QPainter, QColor
+
+
+class Example(QWidget, Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.f(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def f(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        t = random.randint(0, 200)
+        qp.drawEllipse(random.randint(0, 300), random.randint(0, 300), t, t)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
+
